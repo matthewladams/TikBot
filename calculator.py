fd2ab1ea154e9a8ba6d1b8ce24d99c33b744ba80
Matthew Adams <matt@matthewladams.com>
2021-09-27 18:47:17 +1000

    Drop bitrate to allow for more complex videos

--- calculator.py
+++ calculator.py
@@ -14,7 +14,7 @@ def calculateBitrate(duration: int) -> CalculationResult:
     bitrateKilobits = result.videoBitrate
     # Calc video first
     if(duration != 0):
-        bitrateKilobits = (7000 * 8)/duration
+        bitrateKilobits = (6700 * 8)/duration
         result.videoBitrate = round(bitrateKilobits)
 
     # Don't ever drop to full potato
@@ -24,7 +24,7 @@ def calculateBitrate(duration: int) -> CalculationResult:
     
     # Now calculate audio based on our remaining bitrate  
     if(duration != 0):
-        remainingKiloBytes = 7800 - (result.videoBitrate/8) * duration
+        remainingKiloBytes = 7700 - (result.videoBitrate/8) * duration
         print("Bits remaining for audio= " + str(remainingKiloBytes))
         calcedAudioBitrate = (remainingKiloBytes*8)/duration
         roundedCalcedBitrate = round(calcedAudioBitrate)
